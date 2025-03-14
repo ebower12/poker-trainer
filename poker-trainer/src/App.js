9f22a82555ce9849089cf6dd5c92eb630e7aed03
ebower12 <ethanbower@gmail.com>
2025-03-14 10:53:02 -0400

    add table and phases

--- poker-trainer/src/App.js
+++ poker-trainer/src/App.js
@@ -5,20 +5,89 @@ import Dealer from "./dealer";
 
 function App() {
   const [hand, updateHand] = useState([]);
+  const [tableCards, updateTableCards] = useState([]);
+  const [isDeckEmpty, setIsDeckEmpty] = useState(false);
+  const [currentPhase, setCurrentPhase] = useState("pre-flop");
   const [dealer] = useState(new Dealer());
 
-  function dealCards(numberOfCards) {
+  function dealHand() {
     const newHand = [];
-    for (let i = 0; i < numberOfCards; i++) {
-      newHand.push(dealer.deal());
+    for (let i = 0; i < 2; i++) {
+      const nextCard = dealer.deal();
+
+      if (nextCard === "No more cards in the deck") {
+        setIsDeckEmpty(true);
+        break;
+      }
+
+      newHand.push(nextCard);
     }
+
     updateHand(newHand);
+  }
+
+  function dealTable() {
+    const newTable = tableCards;
+    let numberOfCards = 2;
+    let nextPhase = currentPhase;
+
+    switch (currentPhase) {
+      case "pre-flop":
+        numberOfCards = 2;
+        nextPhase = "flop";
+        break;
+      case "flop":
+        numberOfCards = 1;
+        nextPhase = "turn";
+        break;
+      case "turn":
+        numberOfCards = 1;
+        nextPhase = "river";
+        break;
+      case "river":
+        numberOfCards = 0;
+        nextPhase = "pre-flop";
+        break;
+      default:
+        break;
+    }
+
+    for (let i = 0; i < numberOfCards; i++) {
+      const nextCard = dealer.deal();
+
+      if (nextCard === "No more cards in the deck") {
+        setIsDeckEmpty(true);
+        break;
+      }
+
+      newTable.push(nextCard);
+    }
+
+    updateTableCards(newTable);
+    setCurrentPhase(nextPhase);
   }
 
   function resetDeck() {
     dealer.reset();
+    setIsDeckEmpty(false);
+    setCurrentPhase("pre-flop");
     updateHand([]);
+    updateTableCards([]);
   }
+
+  const renderCards = (cards) => {
+    return cards.map((card) => (
+      <span
+        key={card}
+        style={{
+          margin: "0 5px",
+          color: card.includes("♥") || card.includes("♦") ? "red" : "black",
+        }}
+      >
+        {card}
+      </span>
+    ));
+  };
 
   return (
     <div className="App">
@@ -26,8 +95,15 @@ function App() {
         <h1>Poker Trainer</h1>
         <p>Let's learn poker!</p>
         <div style={{ display: "flex", gap: "1rem" }}>
-          <Button variant="primary" onClick={() => dealCards(2)}>
-            Deal
+          <Button variant="primary" onClick={() => dealHand()}>
+            Deal Hand
+          </Button>
+          <Button
+            variant="success"
+            disabled={currentPhase === "river"}
+            onClick={() => dealTable()}
+          >
+            Deal Table
           </Button>
           <Button variant="secondary" onClick={() => dealer.shuffle()}>
             Shuffle
@@ -36,19 +112,18 @@ function App() {
             Reset
           </Button>
         </div>
-        <p>
-          {hand.map((card) => (
-            <span
-              key={card}
-              style={{
-                color:
-                  card.includes("♥") || card.includes("♦") ? "red" : "black",
-              }}
-            >
-              {card}
-            </span>
-          ))}
-        </p>
+        <div>
+          <h2>{`Current phase: ${currentPhase}`}</h2>
+          {isDeckEmpty && <p>No more cards in the deck!</p>}
+          <p>
+            Table:
+            {renderCards(tableCards)}
+          </p>
+          <p>
+            Hand:
+            {renderCards(hand)}
+          </p>
+        </div>
       </header>
     </div>
   );
